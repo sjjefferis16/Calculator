7cfa0a5fd0234260d3e2a9ce1b878a89a36123ab
sjjefferis16 <sjjefferis16@gmail.com>
2020-11-05 23:26:55 -0500

    move buttons

---
+++ buttons.js
@@ -0,0 +1,50 @@
+var one = document.createElement("Button");
+one.innerHTML = "1";
+one.className = "numButton3";
+document.body.appendChild(one);
+
+var two = document.createElement("Button");
+two.innerHTML = "2";
+two.className = " numButton3"
+document.body.appendChild(two);
+
+var three = document.createElement("Button");
+three.innerHTML = "3";
+three.className = "numButton3";
+document.body.appendChild(three);
+
+var four = document.createElement("Button");
+four.innerHTML = "4";
+four.className = "numButton2";
+document.body.appendChild(four);
+
+var five = document.createElement("Button");
+five.innerHTML = "5";
+five.className = "numButton2";
+document.body.appendChild(five);
+
+var six = document.createElement("Button");
+six.innerHTML = "6";
+six.className = "numButton2";
+document.body.appendChild(six);
+
+var seven = document.createElement("Button");
+seven.innerHTML = "7";
+seven.className = "numButton1";
+document.body.appendChild(seven);
+
+var eight = document.createElement("Button");
+eight.innerHTML = "8";
+eight.className = " numButton1";
+document.body.appendChild(eight);
+
+var nine = document.createElement("Button");
+nine.innerHTML = "9";
+nine.className = " numButton1";
+document.body.appendChild(nine);
+
+var zero = document.createElement("Button");
+zero.innerHTML = "0";
+zero.style.backgroundColor="#00eaa1";
+zero.style.height = "60px";
+document.body.appendChild(zero);
