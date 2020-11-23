3a80e0db1d41efdb1896cdf8c21f47e398016adc
WhitePolaris <70379821+WhitePolaris@users.noreply.github.com>
2020-11-23 15:05:34 -0500

    Update calculus.js

--- calculus.js
+++ calculus.js
@@ -27,61 +27,93 @@ document.getElementById("clear").addEventListener("click", function(){ clearFun(
 function calcFun(){
 	// parser goes here
 	// takes in a string an then analysises it and spits out errors if it doesn check out
+
+/*
+Takes in a stirng *it's fuckign RAW*.
+looks at each char to turn into a object, if it's a number then it isolates it. 
+/ mini
+->gets next thing. makes it a object, appends it, and moves on. 
+-> checks whole contrsturct for lexical correctness
+
+objects: 
+L paren
+R paren
+symbol
+number
+operation
+
+theese are appened then string like to an array or stream(>>what do stieams so)
+
+//for futher specs insert lexems into array at start of the lexem, then shrink array by removing all white space..(helper funct by javascript)
+
+*/
 function analysises(str) {
     i = str.length;
     c = 0;
 
+	myNumStr;        
+	perBool = false;
 while (c < i) {
-        //read in c(string pos)
+		//read in c(string pos)
+		
+    switch(str[c]){
 
-        //find if num before
-        mynumstr;        
-        perbool = false;
-        while (str[c].isNum()) {
+		//find if num before
+		//NUMBER  idk if int here works.. rollign on 
+       case int: case '.': while (str[c].isNum()) {
 
             switch (str[c]) {  //next c //if nan or ws exit this 
-                case '.': if (perbool == true) { throw "NaN error: multiple '.'s." } perbool = true;
-                    mynumstr.append('.'); break;
-                case '1': mynumstr.append('1'); break;
-                case '2': mynumstr.append('2'); break;
-                case '3': mynumstr.append('3'); break;
-                case '4': mynumstr.append('4'); break;
-                case '5': mynumstr.append('5'); break;
-                case '6': mynumstr.append('6'); break;
-                case '7': mynumstr.append('7'); break;
-                case '8': mynumstr.append('8'); break;
-                case '9': mynumstr.append('9'); break;
-                case '0': mynumstr.append('0'); break;
+                case '.': if (perBool == true) { throw "NaN error: multiple '.'s." } perBool = true;
+                          myNumStr.append('.'); break;
+                case '1': myNumStr.append('1'); break;
+                case '2': myNumStr.append('2'); break;
+                case '3': myNumStr.append('3'); break;
+                case '4': myNumStr.append('4'); break;
+                case '5': myNumStr.append('5'); break;
+                case '6': myNumStr.append('6'); break;
+                case '7': myNumStr.append('7'); break;
+                case '8': myNumStr.append('8'); break;
+                case '9': myNumStr.append('9'); break;
+                case '0': myNumStr.append('0'); break;
                 default:
-                    break;
+                    break;//to get out of thing (if this was no hte last it would qork with the rest, by asdding the number to the lexigram)
 
             }
             c++;
+		
+		//finally it appends numberOBJ(stringToFloat(myNumStr)); to the structure
+		appendNumber(stringToFloat(myNumStr));
+		//myNumStr = "";
+		}
+		//symbol
+        case 'e': appendSymbolE(); break;
+		case 'π': appendSymbolPi(); break;
 
-        }
-        
+		//Operations
+		case '^': appendOperatorPower(); break;
+		case '/': appendOperatorDiv(); break;
+		case '*': appendOperatorTimes(); break;
+		case '+': appendOperatorPlus(); break;
+		case '-': appendOperatorMinus(); break;
+	
+		//PARENS
+		case '(': appendParenR(); break;
+		case ')': appendParenL(); break;
+	}
+			c++;
     
 //new num added to lex and other stuff is skipped
 
-// is op/ws/sybmbol
- else if (str.hasNextchar()) {
-            ' '
-        }
-        if operation match case
-
-        if symbol  if p get next i
-
- //eiter π or e
- else if e then good.c
- else end
-        //turn to add to string(append)
-    }
 }
 
+
+//checks if num there. sif so add to end ?? >>extra.
 }
+//match l r parens, basicly )( , (().  should fail but ( ) pass   also if we shall allow for haning ()
+//check for double opperations, and fail
+//operation and ) should fail but oper and ( is ok
 
 }
-
 function clearFun(){
 	exp = "";
 	ans = 0.0
